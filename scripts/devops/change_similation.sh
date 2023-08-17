5a5e1e4cb8ab2d2d58f237c9c1fc3b9451b2a33d
Pierre Ndiaye <pierrendiaye@gmail.com>
2023-08-17 20:17:46 +0200

    parallelisme stages

---
+++ scripts/devops/change_similation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "test `date`" >> spring-petclinic-admin-server/triggers.txt
+echo "test `date`" >> spring-petclinic-api-gateway/triggers.txt
+echo "test `date`" >> spring-petclinic-config-server/triggers.txt
+echo "test `date`" >> spring-petclinic-customers-service/triggers.txt
+echo "test `date`" >> spring-petclinic-discovery-server/triggers.txt
+echo "test `date`" >> spring-petclinic-vets-service/triggers.txt
+echo "test `date`" >> spring-petclinic-visits-service/triggers.txt
