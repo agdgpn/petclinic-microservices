92ccccd215c39d7d210d77ee172711cb996f31da
Pierre Ndiaye <pierrendiaye@gmail.com>
2023-08-19 18:53:01 +0200

    amelioration script setup après installation nouveau cluster

--- scripts/devops/setup_env.sh
+++ scripts/devops/setup_env.sh
@@ -3,7 +3,7 @@
 # Prépare l environnment
 ENV_NAME=$1
 ns_status=$(kubectl get ns $ENV_NAME -o json | jq .status.phase -r)
-echo -e "$ns_status"
+
 if [[ $ns_status != "Active" ]]
 then
     echo "Creating namespace $ENV_NAME ..."
@@ -16,33 +16,41 @@ sed -i "s+namespace:.*+namespace: ${ENV_NAME}+g" pipeline/jenkins/agent/secret.y
 
 kubectl apply -f pipeline/jenkins/agent/service-account.yaml
 kubectl apply -f pipeline/jenkins/agent/secret.yaml
+# Verifie si traefik est installe
+traefik_status=$(kubectl get ns traefik -o json | jq .status.phase -r)
 
-# Installation traefik dans le namespace dev avec helm
-# 1. Ajouter et mise a jour repo helm
-helm repo add traefik https://helm.traefik.io/traefik
-helm repo update
-# 2. Creation fichier values.yaml
-echo '# values.yaml' > values.yaml
-echo '---' >> values.yaml
-echo 'ingressClass:' >> values.yaml
-echo '  enabled: true' >> values.yaml
-echo '  isDefaultClass: true' >> values.yaml
-echo '  fallbackApiVersion: v1' >> values.yaml
-echo 'ingressRoute:' >> values.yaml
-echo '  dashboard:' >> values.yaml
-echo '    enabled: false' >> values.yaml
-echo 'service:' >> values.yaml
-echo '  annotations:' >> values.yaml
-echo '    service.beta.kubernetes.io/aws-load-balancer-type: nlb' >> values.yaml
-echo 'globalArguments:' >> values.yaml
-echo '- "--api.insecure=true"' >> values.yaml
-echo 'ports:' >> values.yaml
-echo '  websecure:' >> values.yaml
-echo '    tls:' >> values.yaml
-echo '      enabled: true' >> values.yaml
-# 10.c - Installation traefik dans le namespace traefik
-helm uninstall traefik --namespace=traefik
-helm install traefik traefik/traefik --create-namespace --namespace=traefik --values=values.yaml
-
+if [[ $traefik_status != "Active" ]]
+then
+    echo "Installing traefik ingress controller ..."
+    # Installation traefik dans le namespace dev avec helm
+    # 1. Ajouter et mise a jour repo helm
+    helm repo add traefik https://helm.traefik.io/traefik
+    helm repo update
+    # 2. Creation fichier values.yaml
+    echo '# values.yaml' > values.yaml
+    echo '---' >> values.yaml
+    echo 'ingressClass:' >> values.yaml
+    echo '  enabled: true' >> values.yaml
+    echo '  isDefaultClass: true' >> values.yaml
+    echo '  fallbackApiVersion: v1' >> values.yaml
+    echo 'ingressRoute:' >> values.yaml
+    echo '  dashboard:' >> values.yaml
+    echo '    enabled: false' >> values.yaml
+    echo 'service:' >> values.yaml
+    echo '  annotations:' >> values.yaml
+    echo '    service.beta.kubernetes.io/aws-load-balancer-type: nlb' >> values.yaml
+    echo 'globalArguments:' >> values.yaml
+    echo '- "--api.insecure=true"' >> values.yaml
+    echo 'ports:' >> values.yaml
+    echo '  websecure:' >> values.yaml
+    echo '    tls:' >> values.yaml
+    echo '      enabled: true' >> values.yaml
+    # 10.c - Installation traefik dans le namespace traefik
+    #helm uninstall traefik --namespace=traefik
+    helm install traefik traefik/traefik --create-namespace --namespace=traefik --values=values.yaml
+else
+    echo "Traefik ingress controller is already installed"
+fi
+# Installation du middleware traefik
 kubectl apply -f kubernetes/standard/middleware.yml -n traefik
 
